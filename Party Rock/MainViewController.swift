d13e4158f18f29fe3223fc39637dad8ada5e82c5
Douglas Heitner <douglas.heitner@gmail.com>
2017-02-06 15:07:09 -0800

    Implemented pictures from web.

--- Party Rock/MainViewController.swift
+++ Party Rock/MainViewController.swift
@@ -14,12 +14,29 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var partyRocks = [PartyRock]()
     
-    let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let p1 = PartyRock(imageURL: <#T##String#>, videoURL: <#T##String#>, videoTitle: <#T##String#>)
+        let p1 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/a/a7/JohnMayerBattleStudies.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Crossroads")
+        let p2 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/a/a7/JohnMayerBattleStudies.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Edge of Desire")
+        let p3 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/8/82/Continuum_(album).png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Belief")
+        let p4 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/8/82/Continuum_(album).png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Slow Dancing in a Burning Room")
+        let p5 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/8/82/Continuum_(album).png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Gravity")
+        let p6 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/a/a7/JohnMayerBattleStudies.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Friends, Lovers or Nothing")
+        let p7 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/a/a7/JohnMayerBattleStudies.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Half of My Heart")
+        let p8 = PartyRock(imageURL: "https://upload.wikimedia.org/wikipedia/en/a/a7/JohnMayerBattleStudies.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/aIslBlTkdWk\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Assassin")
+        
+        
+        partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+        partyRocks.append(p5)
+        partyRocks.append(p6)
+        partyRocks.append(p7)
+        partyRocks.append(p8)
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -31,9 +48,9 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartCell", for: indexPath) as? PartyCell{
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath) as? PartyViewCell{
             
-            let partyRock = PartyRock[indexPath.row]
+            let partyRock = partyRocks[indexPath.row]
             
             cell.updateUI (partyRock: partyRock)
             
--- Party Rock/PartyViewCell.swift
+++ Party Rock/PartyViewCell.swift
@@ -21,7 +21,22 @@ class PartyViewCell: UITableViewCell {
     
     func updateUI(partyRock: PartyRock){
         videoTitle.text = partyRock.videoTitle
-        //TODO: Set up image hooks
+
+        let url = URL(string: partyRock.imageURL)
+        
+        DispatchQueue.global().async {
+            do{
+                let data = try Data(contentsOf: url!)
+                
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            }
+            
+            catch {
+                //Handle Error
+            }
+        }
     }
 
 }
